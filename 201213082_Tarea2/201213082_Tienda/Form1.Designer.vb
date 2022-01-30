9cadfd826a0d48e7b1e99e8e6dc8f7f11c26684a
Joselyn092 <98678460+Joselyn092@users.noreply.github.com>
2022-01-29 22:31:31 -0600

    Add files via upload

---
+++ 201213082_Tarea2/201213082_Tienda/Form1.Designer.vb
@@ -0,0 +1,291 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Txtazucar = New System.Windows.Forms.TextBox()
+        Me.Txtfrijol = New System.Windows.Forms.TextBox()
+        Me.Txtarroz = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.LblR5 = New System.Windows.Forms.Label()
+        Me.LblR4 = New System.Windows.Forms.Label()
+        Me.LblR3 = New System.Windows.Forms.Label()
+        Me.LblR2 = New System.Windows.Forms.Label()
+        Me.LblR1 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Btnaceptar = New System.Windows.Forms.Button()
+        Me.BtnLimpiar = New System.Windows.Forms.Button()
+        Me.BtnSalir = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Txtazucar)
+        Me.GroupBox1.Controls.Add(Me.Txtfrijol)
+        Me.GroupBox1.Controls.Add(Me.Txtarroz)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(25, 34)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(326, 154)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Datos de ingreso"
+        '
+        'Txtazucar
+        '
+        Me.Txtazucar.Location = New System.Drawing.Point(162, 109)
+        Me.Txtazucar.Name = "Txtazucar"
+        Me.Txtazucar.Size = New System.Drawing.Size(148, 27)
+        Me.Txtazucar.TabIndex = 5
+        '
+        'Txtfrijol
+        '
+        Me.Txtfrijol.Location = New System.Drawing.Point(162, 75)
+        Me.Txtfrijol.Name = "Txtfrijol"
+        Me.Txtfrijol.Size = New System.Drawing.Size(148, 27)
+        Me.Txtfrijol.TabIndex = 4
+        '
+        'Txtarroz
+        '
+        Me.Txtarroz.Location = New System.Drawing.Point(162, 41)
+        Me.Txtarroz.Name = "Txtarroz"
+        Me.Txtarroz.Size = New System.Drawing.Size(148, 27)
+        Me.Txtarroz.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 112)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(116, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Libras de azúcar"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(104, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Libras de frijol"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Libras de arroz"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.LblR5)
+        Me.GroupBox2.Controls.Add(Me.LblR4)
+        Me.GroupBox2.Controls.Add(Me.LblR3)
+        Me.GroupBox2.Controls.Add(Me.LblR2)
+        Me.GroupBox2.Controls.Add(Me.LblR1)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Location = New System.Drawing.Point(422, 45)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(307, 269)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Resultados en quetzales (Q)"
+        '
+        'LblR5
+        '
+        Me.LblR5.AutoSize = True
+        Me.LblR5.Location = New System.Drawing.Point(190, 233)
+        Me.LblR5.Name = "LblR5"
+        Me.LblR5.Size = New System.Drawing.Size(28, 20)
+        Me.LblR5.TabIndex = 9
+        Me.LblR5.Text = "0.0"
+        '
+        'LblR4
+        '
+        Me.LblR4.AutoSize = True
+        Me.LblR4.Location = New System.Drawing.Point(190, 191)
+        Me.LblR4.Name = "LblR4"
+        Me.LblR4.Size = New System.Drawing.Size(28, 20)
+        Me.LblR4.TabIndex = 8
+        Me.LblR4.Text = "0.0"
+        '
+        'LblR3
+        '
+        Me.LblR3.AutoSize = True
+        Me.LblR3.Location = New System.Drawing.Point(190, 136)
+        Me.LblR3.Name = "LblR3"
+        Me.LblR3.Size = New System.Drawing.Size(28, 20)
+        Me.LblR3.TabIndex = 7
+        Me.LblR3.Text = "0.0"
+        '
+        'LblR2
+        '
+        Me.LblR2.AutoSize = True
+        Me.LblR2.Location = New System.Drawing.Point(190, 83)
+        Me.LblR2.Name = "LblR2"
+        Me.LblR2.Size = New System.Drawing.Size(28, 20)
+        Me.LblR2.TabIndex = 6
+        Me.LblR2.Text = "0.0"
+        '
+        'LblR1
+        '
+        Me.LblR1.AutoSize = True
+        Me.LblR1.Location = New System.Drawing.Point(190, 38)
+        Me.LblR1.Name = "LblR1"
+        Me.LblR1.Size = New System.Drawing.Size(28, 20)
+        Me.LblR1.TabIndex = 5
+        Me.LblR1.Text = "0.0"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(22, 233)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(75, 20)
+        Me.Label8.TabIndex = 4
+        Me.Label8.Text = "Pago final"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(22, 191)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(79, 20)
+        Me.Label7.TabIndex = 3
+        Me.Label7.Text = "Descuento"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(22, 136)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(96, 20)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "Pago con IVA"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(22, 83)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(90, 20)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Valor de IVA"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(22, 38)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(128, 20)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Pago antes de IVA"
+        '
+        'Btnaceptar
+        '
+        Me.Btnaceptar.Location = New System.Drawing.Point(109, 207)
+        Me.Btnaceptar.Name = "Btnaceptar"
+        Me.Btnaceptar.Size = New System.Drawing.Size(165, 28)
+        Me.Btnaceptar.TabIndex = 2
+        Me.Btnaceptar.Text = "Aceptar"
+        Me.Btnaceptar.UseVisualStyleBackColor = True
+        '
+        'BtnLimpiar
+        '
+        Me.BtnLimpiar.Location = New System.Drawing.Point(109, 252)
+        Me.BtnLimpiar.Name = "BtnLimpiar"
+        Me.BtnLimpiar.Size = New System.Drawing.Size(165, 28)
+        Me.BtnLimpiar.TabIndex = 3
+        Me.BtnLimpiar.Text = "Limpiar"
+        Me.BtnLimpiar.UseVisualStyleBackColor = True
+        '
+        'BtnSalir
+        '
+        Me.BtnSalir.Location = New System.Drawing.Point(109, 301)
+        Me.BtnSalir.Name = "BtnSalir"
+        Me.BtnSalir.Size = New System.Drawing.Size(165, 28)
+        Me.BtnSalir.TabIndex = 4
+        Me.BtnSalir.Text = "Salir"
+        Me.BtnSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Info
+        Me.ClientSize = New System.Drawing.Size(800, 355)
+        Me.Controls.Add(Me.BtnSalir)
+        Me.Controls.Add(Me.BtnLimpiar)
+        Me.Controls.Add(Me.Btnaceptar)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Txtazucar As TextBox
+    Friend WithEvents Txtfrijol As TextBox
+    Friend WithEvents Txtarroz As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents LblR5 As Label
+    Friend WithEvents LblR4 As Label
+    Friend WithEvents LblR3 As Label
+    Friend WithEvents LblR2 As Label
+    Friend WithEvents LblR1 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Btnaceptar As Button
+    Friend WithEvents BtnLimpiar As Button
+    Friend WithEvents BtnSalir As Button
+End Class
---
+++ 201213082_Tarea2/201213082_Tienda/Form1.vb
@@ -0,0 +1,78 @@
+﻿Public Class Form1
+
+    Const precioarroz As Double = 2.0
+    Const preciofrijol As Double = 1.75
+    Const precioazucar As Double = 2.5
+    Const IVA As Double = 0.12
+
+    Dim pagoSinIva As Double = 0
+    Dim valorIVA As Double = 0
+    Dim pagoconIVA As Double = 0
+    Dim descuento As Double = 0
+    Dim pagofinal As Double = 0
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
+
+    End Sub
+
+    Private Sub Txtarroz_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Txtarroz.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub Txtfrijol_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Txtfrijol.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub Txtazucar_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Txtazucar.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub Btnaceptar_Click(sender As Object, e As EventArgs) Handles Btnaceptar.Click
+        pagoSinIva = Txtarroz.Text * precioarroz + Txtfrijol.Text * preciofrijol + Txtazucar.Text * precioazucar
+        valorIVA = pagoSinIva * IVA
+        pagoconIVA = pagoSinIva + valorIVA
+        descuento = pagoconIVA * 0.025
+        pagofinal = pagoconIVA - descuento
+
+        LblR1.Text = pagoSinIva
+        LblR2.Text = valorIVA
+        LblR3.Text = pagoconIVA
+        LblR4.Text = descuento
+        LblR5.Text = pagofinal
+
+    End Sub
+
+    Private Sub BtnLimpiar_Click(sender As Object, e As EventArgs) Handles BtnLimpiar.Click
+        Txtarroz.Clear()
+        Txtazucar.Clear()
+        Txtfrijol.Clear()
+        LblR1.Text = 0.0
+        LblR2.Text = 0.0
+        LblR3.Text = 0.0
+        LblR4.Text = 0.0
+        LblR5.Text = 0.0
+
+
+
+    End Sub
+
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles BtnSalir.Click
+        Close()
+    End Sub
+End Class
